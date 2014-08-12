f6b0659b02592f3556ed5a16fb668df0ba6aabed
Andre Bandarra <andreban@gmail.com>
2014-08-12 15:19:09 -0300

    First Working version

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Repository for the Course Project on the Getting and Cleaning Data course from C
 Instructions:
 - Download the raw data file from the Course Project page (https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
 - Unzip the content inside a directory named ".data" inside the project root
+
+
+Methodology
+===========
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,49 @@
-readFiles <- function() {
-    testSetFileName <- "./data/UCI HAR Dataset/test/head_X_test_sample.txt";
-    print(testSetFileName)
+readData <- function(subjectsFileName, featuresFileName, columnIds) {
+    rows <- -1;
+    fields <- rep(-16, 561)
+    fields[columnIds] <- fields[columnIds] * -1
     testSet <- read.fwf(
-        file = testSetFileName, 
-        header = FALSE, 
-        widths = c(16, 16)
+        file = featuresFileName, 
+        header = FALSE,
+        widths = fields,
+        n=rows
     )
-    #testSet <- read.delim(file = trainingSetFileName, header = FALSE, sep = " ")
-    summary(testSet)
-    head(testSet, n = 1)
     
-
+    testSubjects <- read.delim(file = subjectsFileName, header=FALSE, nrows=rows)    
+    testData <- cbind(testSet, as.factor(testSubjects[,1]))     
+    testData    
 }
-?read.fwf
-?read.delim
+
+buildFullDataSet <- function() {
+    featuresFileName <- "./data/UCI HAR Dataset/features.txt"
+    features <- read.delim(file=featuresFileName, header=FALSE, sep=" ")    
+    meanFields <- grep("\\-mean\\(\\)", features[,2])
+    stdFields <- grep("\\-std\\(\\)", features[,2])
+    
+    targedFields <- sort(c(meanFields, stdFields))
+        
+    testSetFileName <- "./data/UCI HAR Dataset/test/X_test.txt";
+    testSubjectFileName <- "./data/UCI HAR Dataset/test/subject_test.txt";
+    trainSetfileName <- "./data/UCI HAR Dataset/train/X_train.txt";
+    trainSubjectFileName <- "./data/UCI HAR Dataset/train/subject_train.txt";    
+    
+    testData <- readData(testSubjectFileName, testSetFileName, features[targedFields,1])    
+    traindata <- readData(trainSubjectFileName, trainSetfileName, features[targedFields,1])        
+    fullDataSet <- rbind(testData, traindata)
+        
+    colnames(fullDataSet) <- c(as.character(features[targedFields,2]), "Subject")
+    colnames(fullDataSet)
+    fullDataSet    
+}
+
+applyMean <- function(dataset) {
+    sapply(dataset, mean)
+}
+
+splitData <- split(fullDataSet, fullDataSet$Subject)
+averagedList <- lapply(splitData, applyMean)
+tidy <- sapply(averagedList, c)
+tidy <- tidy[1:dim(tidy)[1]-1,]
+
+write.table(tidy, file="./data/tidy.txt")
+
