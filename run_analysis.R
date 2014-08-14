f76858e7c0b3dfc0a9bc1642bef764532d5b1976
Andre Bandarra <andreban@gmail.com>
2014-08-14 15:09:49 -0300

    Working code

--- run_analysis.R
+++ run_analysis.R
@@ -1,49 +1,78 @@
-readData <- function(subjectsFileName, featuresFileName, columnIds) {
-    rows <- -1;
-    fields <- rep(-16, 561)
+library(plyr)
+#This method downloads the data file an extracts it to the .data directory
+fetchData <- function() {
+    if (!file.exists("./data")) {
+        dir.create("./data")
+    }
+    if (!file.exists("./data/UCI HAR Dataset")) {
+        fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"                            
+        zipfile <- "./data/UCI_HAR_data.zip"
+        download.file(fileURL, destfile=zipfile, method="curl")
+        unzip(zipfile, exdir="./data")
+    }
+}
+
+#This method reads data from the measurements file, subjects file and activities file and merges them in a single data frame
+readData <- function(subjectsFileName, featuresFileName, activitiesFilename, columnIds) {
+    #The measurements files have 561 fields, each one with a length of 16 characters
+    fields <- rep(-16, 561) 
     fields[columnIds] <- fields[columnIds] * -1
     testSet <- read.fwf(
         file = featuresFileName, 
         header = FALSE,
-        widths = fields,
-        n=rows
+        widths = fields
     )
     
-    testSubjects <- read.delim(file = subjectsFileName, header=FALSE, nrows=rows)    
-    testData <- cbind(testSet, as.factor(testSubjects[,1]))     
+    testSubjects <- read.delim(file = subjectsFileName, header=FALSE, stringsAsFactors=FALSE)    
+    activities <- read.delim(file = activitiesFilename, sep=" ", header=FALSE)
+    testData <- cbind(testSet, testSubjects[,1], activities[,1])     
     testData    
 }
 
-buildFullDataSet <- function() {
-    featuresFileName <- "./data/UCI HAR Dataset/features.txt"
+getMeanAndStdFields <- function() {
+    featuresFileName <- "./data/UCI HAR Dataset/features.txt"    
     features <- read.delim(file=featuresFileName, header=FALSE, sep=" ")    
     meanFields <- grep("\\-mean\\(\\)", features[,2])
     stdFields <- grep("\\-std\\(\\)", features[,2])
     
-    targedFields <- sort(c(meanFields, stdFields))
-        
+    targedFields <- sort(c(meanFields, stdFields))    
+    targedFields    
+}
+
+getActivitiesLabels <- function() {
+    activityLabelsFileName <- "./data/UCI HAR Dataset/activity_labels.txt"    
+    activityLabels <- read.delim(file=activityLabelsFileName, header=FALSE, sep= " ")    
+    activityLabels    
+}
+
+buildFullDataSet <- function() {
     testSetFileName <- "./data/UCI HAR Dataset/test/X_test.txt";
     testSubjectFileName <- "./data/UCI HAR Dataset/test/subject_test.txt";
+    testSetActivitiesFileName <- "./data/UCI HAR Dataset/test/y_test.txt"
+    
     trainSetfileName <- "./data/UCI HAR Dataset/train/X_train.txt";
     trainSubjectFileName <- "./data/UCI HAR Dataset/train/subject_train.txt";    
-    
-    testData <- readData(testSubjectFileName, testSetFileName, features[targedFields,1])    
-    traindata <- readData(trainSubjectFileName, trainSetfileName, features[targedFields,1])        
-    fullDataSet <- rbind(testData, traindata)
+    trainSetActivitiesFileName <- "./data/UCI HAR Dataset/train/y_train.txt"    
         
-    colnames(fullDataSet) <- c(as.character(features[targedFields,2]), "Subject")
+    targedFields <- getMeanAndStdFields()
+    
+    testData <- readData(testSubjectFileName, testSetFileName, testSetActivitiesFileName, features[targedFields,1])    
+    
+    trainData <- readData(trainSubjectFileName, trainSetfileName, trainSetActivitiesFileName, features[targedFields,1])        
+    
+    fullDataSet <- rbind(testData, trainData)
+    
+        
+    colnames(fullDataSet) <- c(as.character(features[targedFields,2]), "Subject", "ActivityID")
     colnames(fullDataSet)
     fullDataSet    
 }
 
-applyMean <- function(dataset) {
-    sapply(dataset, mean)
-}
+fullDataSet <- buildFullDataSet();
+activityLabels <- getActivitiesLabels()
 
-splitData <- split(fullDataSet, fullDataSet$Subject)
-averagedList <- lapply(splitData, applyMean)
-tidy <- sapply(averagedList, c)
-tidy <- tidy[1:dim(tidy)[1]-1,]
-
+fullDataSet <- merge(fullDataSet, activityLabels, by.x="ActivityID", by.y="V1")
+tidy <- ddply(fullDataSet, .(Subject, V2), function(x) colMeans(x[,1:66]))
 write.table(tidy, file="./data/tidy.txt")
+
 
