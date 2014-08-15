66b4438d7a4de7b864c00b77912b5a7eab675d47
Andre Bandarra <andreban@gmail.com>
2014-08-15 11:15:10 -0300

    Markdown files and finished run_analysis.R

--- README.md
+++ README.md
@@ -3,12 +3,53 @@ GetDataProject
 
 Repository for the Course Project on the Getting and Cleaning Data course from Coursera (getdata-006)
 
+The objective of this script is to extract information from the UCI HAR Dataset (https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip) and apply transformations to it in order to generate a tidy dataset;
 
-Instructions:
-- Download the raw data file from the Course Project page (https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
-- Unzip the content inside a directory named ".data" inside the project root
+This repository contains a single R script `run_analysis.R`. In order to download and generate the tidy dataset, one should source the `run_analysis.R` and then run the `buildTidyDataSet()` function. The script is able to download and extract the dataSet by itself. But you can also manually download and extract the information to the `/data` directory.
+
+More information on the resulting tidy dataset can be found on the `CodeBook.md` file
 
 
-Methodology
-===========
+#Objectives
+
+- Merges the training and the test sets to create one data set.
+- Extracts only the measurements on the mean and standard deviation for each measurement. 
+- Uses descriptive activity names to name the activities in the data set
+- Appropriately labels the data set with descriptive variable names. 
+- Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+
+
+#The UCI HAR Dataset
+
+The UCI HAR Dataset contains both a training dataset and a test dataset. Each set has 3 files. 
+
+- One that contains the measurements
+- A second one that contains the subject id
+- A third one that contains the activity ID. 
+
+Also, on the root directory, there is a file `features.txt` that contains the features descriptions and `activity_labels.txt` that contains the activity descriptions.
+
+
+#Loading the UCI Dataset
+
+As we need only the mean and standart deviation for each measurement, we load the the "features.txt" file in order to extract which field names contain the words `-mean()` and `-std()`. The result is a list containing 66 features (33 with standard deviations and 33 with means)
+
+The next step is to load the load the train and test dataset. The measurements file has 561 fields, each one with 16 characters, so, we can read it with as a fixed file. 
+
+In order save memory, we can use the information extracted from the features file to select which fields to load. The next steps are to load the subjects and activity ids from the dataset and combine them with the measurements.
+
+After loading both the train and test datasets, the next step is joining them together.
+
+Next, we give descriptive column names for the information loaded from the datasets    
+
+With the full dataset ready, we load the activity descriptions and join them with the full dataset. 
+
+The last step for generating the full dataset is dropping the activity id column.
+    
+#Generating the Tidy dataset.
+
+With the full dataset ready, we use the 'plyr' package to generate the means for each mesurement column, grouped by the "Activity" and "Subject" columns. Then we save the dataset to a file on `/data/tidy.txt`
+
+
 
--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,14 @@
+#This scripts downloads an process data from the UCI HAR Dataset in order to generate a new tidy datase
+
 library(plyr)
 #This method downloads the data file an extracts it to the .data directory
 fetchData <- function() {
+    #checks if the .data directory exists. If not, create it
     if (!file.exists("./data")) {
         dir.create("./data")
     }
+    
+    #checks if the UC HAR Dataset folder exists. If not , create it!
     if (!file.exists("./data/UCI HAR Dataset")) {
         fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"                            
         zipfile <- "./data/UCI_HAR_data.zip"
@@ -23,58 +28,85 @@ readData <- function(subjectsFileName, featuresFileName, activitiesFilename, col
         widths = fields
     )
     
+    #Load the subjects file
     testSubjects <- read.delim(file = subjectsFileName, header=FALSE, stringsAsFactors=FALSE)    
+    
+    #Load the activities
     activities <- read.delim(file = activitiesFilename, sep=" ", header=FALSE)
+    
+    #Join the 3 datasets
     testData <- cbind(testSet, testSubjects[,1], activities[,1])     
     testData    
 }
 
+#this method reads the features descriptions from the UCI dataset and extracts the ones that 
+#refer to the mean and std of the measurements
 getMeanAndStdFields <- function() {
     featuresFileName <- "./data/UCI HAR Dataset/features.txt"    
     features <- read.delim(file=featuresFileName, header=FALSE, sep=" ")    
+    
+    #check which fields have mean
     meanFields <- grep("\\-mean\\(\\)", features[,2])
+    
+    #check which fields have std
     stdFields <- grep("\\-std\\(\\)", features[,2])
     
     targedFields <- sort(c(meanFields, stdFields))    
     targedFields    
 }
 
+#This method loads the activity labels from the UCI HAR Dataset
 getActivitiesLabels <- function() {
     activityLabelsFileName <- "./data/UCI HAR Dataset/activity_labels.txt"    
     activityLabels <- read.delim(file=activityLabelsFileName, header=FALSE, sep= " ")    
     activityLabels    
 }
 
+#this method reads both the test dataset and train dataset, select the mean and std columns
 buildFullDataSet <- function() {
+    #the test file names
     testSetFileName <- "./data/UCI HAR Dataset/test/X_test.txt";
     testSubjectFileName <- "./data/UCI HAR Dataset/test/subject_test.txt";
     testSetActivitiesFileName <- "./data/UCI HAR Dataset/test/y_test.txt"
     
+    #the train file names
     trainSetfileName <- "./data/UCI HAR Dataset/train/X_train.txt";
     trainSubjectFileName <- "./data/UCI HAR Dataset/train/subject_train.txt";    
     trainSetActivitiesFileName <- "./data/UCI HAR Dataset/train/y_train.txt"    
         
+    #get which fields to load
     targedFields <- getMeanAndStdFields()
     
+    #load the test data
     testData <- readData(testSubjectFileName, testSetFileName, testSetActivitiesFileName, features[targedFields,1])    
     
+    #load the train data
     trainData <- readData(trainSubjectFileName, trainSetfileName, trainSetActivitiesFileName, features[targedFields,1])        
     
+    #join both datasets
     fullDataSet <- rbind(testData, trainData)
     
-        
+    #Set the column Names
     colnames(fullDataSet) <- c(as.character(features[targedFields,2]), "Subject", "ActivityID")
-    colnames(fullDataSet)
-    fullDataSet    
+    
+    #Load the Activities Labels
+    activityLabels <- getActivitiesLabels()    
+    
+    #Join the Dataset with the Activities names
+    fullDataSet <- merge(fullDataSet, activityLabels, by.x="ActivityID", by.y="V1")
+    colnames(fullDataSet)[length(fullDataSet)] <- "Activity"
+    
+    fullDataSet[,2:length(fullDataSet)] 
 }
 
-fullDataSet <- buildFullDataSet();
-activityLabels <- getActivitiesLabels()
+#This methods builds the tidy dataset.
+buildTidyDataSet <- function() {
+    fetchData()    #Downloads the data if it doesnt exist
+    fullDataSet <- buildFullDataSet(); #Builds the full dataset
+    
+    tidy <- ddply(fullDataSet, .(Subject, Activity), function(x) colMeans(x[,1:66])) #builds the tidy dataset
+    write.table(tidy, file="./data/tidy.txt", row.name=FALSE) #save it to the tidy.txt file    
+}
 
-fullDataSet <- merge(fullDataSet, activityLabels, by.x="ActivityID", by.y="V1")
-tidy <- ddply(fullDataSet, .(Subject, V2), function(x) colMeans(x[,1:66]))
-tidy <- tidy[,(c(1,2,4:68))]
-colnames(tidy)[2] <- "Activity"
-write.table(tidy, file="./data/tidy.txt")
 
 
